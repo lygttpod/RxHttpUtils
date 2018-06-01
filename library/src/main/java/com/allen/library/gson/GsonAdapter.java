359c5dae0fc3f45da885000e6e2b66b7a9db8b0a
xiaoyao <xiaoyao@51vest.com>
2018-06-01 19:06:56 +0800

    优化全局配置okhttp对象

--- library/src/main/java/com/allen/library/gson/GsonAdapter.java
+++ library/src/main/java/com/allen/library/gson/GsonAdapter.java
@@ -10,17 +10,15 @@ import com.google.gson.GsonBuilder;
 public class GsonAdapter {
 
     public static Gson buildGson() {
-        Gson gson = null;
-        if (gson == null) {
-            gson = new GsonBuilder()
-                    .registerTypeAdapter(Integer.class, new IntegerDefault0Adapter())
-                    .registerTypeAdapter(int.class, new IntegerDefault0Adapter())
-                    .registerTypeAdapter(Double.class, new DoubleDefault0Adapter())
-                    .registerTypeAdapter(double.class, new DoubleDefault0Adapter())
-                    .registerTypeAdapter(Long.class, new LongDefault0Adapter())
-                    .registerTypeAdapter(long.class, new LongDefault0Adapter())
-                    .create();
-        }
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Integer.class, new IntegerDefault0Adapter())
+                .registerTypeAdapter(int.class, new IntegerDefault0Adapter())
+                .registerTypeAdapter(Double.class, new DoubleDefault0Adapter())
+                .registerTypeAdapter(double.class, new DoubleDefault0Adapter())
+                .registerTypeAdapter(Long.class, new LongDefault0Adapter())
+                .registerTypeAdapter(long.class, new LongDefault0Adapter())
+                .create();
+
         return gson;
     }
 }
--- library/src/main/java/com/allen/library/http/RetrofitClient.java
+++ library/src/main/java/com/allen/library/http/RetrofitClient.java
@@ -1,8 +1,14 @@
 package com.allen.library.http;
 
+import android.util.Log;
+
+import com.allen.library.config.OkHttpConfig;
 import com.allen.library.gson.GsonAdapter;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -11,8 +17,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created on 2017/5/3.
  *
  * @author Allen
- *         <p>
- *         RetrofitClient工具类
+ * <p>
+ * RetrofitClient工具类
  */
 
 public class RetrofitClient {
@@ -20,15 +26,39 @@ public class RetrofitClient {
     private static RetrofitClient instance;
 
     private Retrofit.Builder mRetrofitBuilder;
-    private OkHttpClient.Builder mOkHttpBuilder;
+
+    private OkHttpClient okHttpClient;
 
     public RetrofitClient() {
 
-        mOkHttpBuilder = HttpClient.getInstance().getBuilder();
+        initDefaultOkHttpClient();
+        okHttpClient = new OkHttpClient.Builder().build();
 
         mRetrofitBuilder = new Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(GsonAdapter.buildGson()));
+    }
+
+    private void initDefaultOkHttpClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        builder.readTimeout(10, TimeUnit.SECONDS);
+        builder.writeTimeout(10, TimeUnit.MILLISECONDS);
+        builder.connectTimeout(10, TimeUnit.MILLISECONDS);
+
+        SSLUtils.SSLParams sslParams = SSLUtils.getSslSocketFactory();
+        builder.sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager);
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Log.e("RxHttpUtils", message);
+            }
+        });
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.addInterceptor(loggingInterceptor);
+
+        okHttpClient = builder.build();
     }
 
 
@@ -51,7 +81,11 @@ public class RetrofitClient {
     }
 
     public Retrofit getRetrofit() {
-        return mRetrofitBuilder.client(mOkHttpBuilder.build()).build();
+        if (null == OkHttpConfig.getOkHttpClient()) {
+            return mRetrofitBuilder.client(okHttpClient).build();
+        } else {
+            return mRetrofitBuilder.client(OkHttpConfig.getOkHttpClient()).build();
+        }
     }
 
 }
