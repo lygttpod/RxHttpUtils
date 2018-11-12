764adad4d01a15e1607ce2f2671be95e029ba497
Allen <lygttpod@163.com>
2018-11-12 19:59:32 +0800

    修复取消请求时候报ConcurrentModificationException异常的bug

--- library/src/main/java/com/allen/library/manage/RxHttpManager.java
+++ library/src/main/java/com/allen/library/manage/RxHttpManager.java
@@ -99,8 +99,15 @@ public class RxHttpManager implements IRxHttpManager<Object> {
         Iterator<Map.Entry<Object, CompositeDisposable>> it = mMaps.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry<Object, CompositeDisposable> entry = it.next();
-            Object tag = entry.getKey();
-            cancel(tag);
+            CompositeDisposable disposable = entry.getValue();
+            //如果直接使用map的remove方法会报这个错误java.util.ConcurrentModificationException
+            //所以要使用迭代器的方法remove
+            if (null != disposable) {
+                if (!disposable.isDisposed()) {
+                    disposable.dispose();
+                    it.remove();
+                }
+            }
         }
     }
 }
