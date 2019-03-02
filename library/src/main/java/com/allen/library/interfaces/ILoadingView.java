2de0879dc92f59f2e93ac8363f2d5e12026ec04e
Allen <lygttpod@163.com>
2019-03-02 11:37:30 +0800

    接口化处理loadingView，突破之前只能用dialog的局限

---
+++ library/src/main/java/com/allen/library/interfaces/ILoadingView.java
@@ -0,0 +1,12 @@
+package com.allen.library.interfaces;
+
+/**
+ * <pre>
+ *      @author : Allen
+ *      e-mail  : lygttpod@163.com
+ *      date    : 2019/03/02
+ *      desc    :
+ * </pre>
+ */
+public class ILoadingView {
+}
